388641037e153ca720432fc512b0c2c72877867c
Anojan Pirabakaran <anojan.pirabakaran725@gmail.com>
2024-04-23 22:47:10 +0200

    delete APIs and spring security

--- ipavorbereitung/src/main/java/abschluss/ano/ipa/ipavorbereitung/config/security/SecurityConfig.java
+++ ipavorbereitung/src/main/java/abschluss/ano/ipa/ipavorbereitung/config/security/SecurityConfig.java
@@ -1,4 +1,5 @@
 package abschluss.ano.ipa.ipavorbereitung.config.security;
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,3 +25,4 @@ public class SecurityConfig {
                 .build();
     }
 }
+*/
